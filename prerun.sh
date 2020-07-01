2ecd4ee32b70dbc227f5cc4b99cf51525cbe7e54
Matt Cain <30560401+thrakc3001@users.noreply.github.com>
2020-07-01 11:23:50 -0500

    Update prerun.sh

--- prerun.sh
+++ prerun.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo "
-185.72.130.41 rewesports2019community.tccglobal.com" >> /etc/hosts
+echo "185.72.130.41 rewesports2019community.tccglobal.com" >> /etc/hosts
 echo "done... verifying entry in hosts file:"
